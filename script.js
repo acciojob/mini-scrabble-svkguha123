b35887ef6cc756a85b8513b5623bb77a5c07036c
Souvik Guha <svkguha123@gmail.com>
2023-04-28 13:52:22 +0530

    up

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+function countLetters() {
+  // Get the text from the textbox
+  const text = document.getElementById('evaluatedText').value;
+
+  // Get the length of the text
+  const length = text.length;
+
+  // Update the letter count in the h3 tag
+  document.getElementById('letterCount').innerText = length;
+}
